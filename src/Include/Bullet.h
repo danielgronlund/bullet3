e1a38487f6b5f788b008692b8472781377240fb5
Daniel Grönlund <just.danne@gmail.com>
2024-08-24 13:07:36 +0200

    Expose multithread headers

--- src/Include/Bullet.h
+++ src/Include/Bullet.h
@@ -3,5 +3,9 @@
 
 #include <../btBulletCollisionCommon.h>
 #include <../btBulletDynamicsCommon.h>
+#include "../BulletCollision/CollisionDispatch/btCollisionDispatcherMt.h"
+#include "../BulletDynamics/Dynamics/btDiscreteDynamicsWorldMt.h"
+#include "../LinearMath/TaskScheduler/btThreadSupportInterface.h"
+#include "../BulletDynamics/ConstraintSolver/btSequentialImpulseConstraintSolverMt.h"
 
 #endif /* Header_h */
